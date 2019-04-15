fb3df9a05060d646feace524fa0ee883e74af081
Jason Snider <jsnider@microtrain.net>
2019-04-15 12:01:28 -0500

    Added the ability to edit a blog post

--- public/posts/view.php
+++ public/posts/view.php
@@ -7,13 +7,24 @@ $args = [
 $input = filter_input_array(INPUT_GET, $args);
 $slug = preg_replace("/[^a-z0-9-]+/", "", $input['slug']);
 
-$content=null;
+
 $stmt = $pdo->prepare('SELECT * FROM posts WHERE slug = ?');
 $stmt->execute([$slug]);
 
 $row = $stmt->fetch();
-$content .= "<h1>{$row['title']}</h1>";
-$content .= $row['body'];
+
+$content=<<<EOT
+<h1>{$row['title']}</h1>
+{$row['body']}
+
+<hr>
+<div>
+    <a href="/posts/edit.php?id={$row['id']}">Edit</a>
+</div>
+EOT;
+
+
+
 require '../../core/layout.php';
 
 
