9595a2aa007b801269dd351681da2b4a376931d4
Jason Snider <jsnider@microtrain.net>
2019-04-17 14:59:02 -0500

    Login implementation

--- public/login.php
+++ public/login.php
@@ -1,21 +1,65 @@
 <?php
 require '../core/session.php';
+require '../core/db_connect.php';
 
-$hash = password_hash('12345', PASSWORD_DEFAULT);
-var_dump(password_verify('12345g',$hash));
+$input = filter_input_array(INPUT_POST,[
+    'email'=>FILTER_SANITIZE_EMAIL,
+    'password'=>FILTER_UNSAFE_RAW
+]);
 
-if(!empty($_POST)){
-    $_SESSION['user'] = [];
-    $_SESSION['user']['id']=12345;
-    header('LOCATION: ' . $_POST['goto']);
+if(!empty($input)){
+
+    $sql='SELECT id, hash FROM users WHERE email=:email';
+
+    $stmt=$pdo->prepare($sql);
+    $stmt->execute([
+        'email'=>$input['email']
+    ]);
+
+    $row=$stmt->fetch();
+
+    if($row){
+        $match = password_verify($input['password'], $row['hash']);
+        if($match){
+            $_SESSION['user'] = [];
+            $_SESSION['user']['id']=$row['id'];
+            header('LOCATION: ' . $_POST['goto']);
+        }
+    }
 }
+
+
+
+
 $meta=[];
 $meta['title']="Login";
 $goto=!empty($_GET['goto'])?$_GET['goto']:'/';
 
 $content=<<<EOT
-<form method="post">
-    <input name="goto" value="{$goto}">
+<h1>{$meta['title']}</h1>
+<form method="post" autocomplete="off">
+
+    <div class="form-group">
+        <label for="email">Email</label>
+        <input 
+            class="form-control"
+            id="email"
+            name="email"
+            type="email"
+        >
+    </div>
+
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input 
+            class="form-control" 
+            id="password" 
+            name="password" 
+            type="password"
+        >
+    </div>
+
+    <input name="goto" value="{$goto}" type="hidden">
     <input type="submit" class="btn btn-primary">
 </form>
 EOT;
